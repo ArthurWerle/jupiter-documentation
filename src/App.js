745a97cf0ce83b4bfe4bbfd951ac8de69e1d0567
arthurwerle <arthur.werle@gmail.com>
2020-05-29 17:12:51 -0300

    template

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          <p>
-            Jupiter UI library
-          </p>
+      <header>
+        <div className="ju-container">
+          <div className="ju-box tabs">
+            <div>Home</div>
+            <div>About</div>
+            <div>Why use jupiter?</div>
+            <div>Atoms Components</div>
+            <div>Molecules Components</div>
+            <div>Organisms Components</div>
+          </div>          
         </div>
       </header>
+      <main className="ju-container">
+        <div className="ju-box">
+          Jupiter UI library
+        </div>
+      </main>
+      <footer>
+        <div>
+          <p>Made with (love) by <a href="www.google.com">me</a></p>
+        </div>
+      </footer>
     </div>
   );
 }
